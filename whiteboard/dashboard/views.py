f07d0af5ef9b6a344c7a49d24340575c6a39d594
Tom Clancy <tclancy@gmail.com>
2015-10-27 14:04:34 -0400

    Weather results

--- whiteboard/dashboard/views.py
+++ whiteboard/dashboard/views.py
@@ -1,5 +1,8 @@
+import pywapi
+
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "dashboard/index.html")
+    results = pywapi.get_weather_from_weather_com("03820", units="imperial")
+    return render(request, "dashboard/index.html", {"forecasts": results.get("forecasts")})
