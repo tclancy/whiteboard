ff4de004fc1cdf1826d4a253829d9b4be79493bd
Tom Clancy <tclancy@gmail.com>
2015-10-27 20:26:33 -0400

    Documentation/ TODO

--- whiteboard/dashboard/events.py
+++ whiteboard/dashboard/events.py
@@ -3,5 +3,9 @@ import eventful
 from django.conf import settings
 
 def get_events():
+    """
+    Docs: https://api.eventful.com/docs/events/search
+    Need to sort by date and then show some actual details
+    """
     api = eventful.API(settings.EVENTFUL_API_KEY)
     return api.call('/events/search', l='Dover, NH').get('events').get('event')
